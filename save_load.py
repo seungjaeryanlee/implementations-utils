2cf877a049a19f7e209b5d8c51bac34d6eddeac2
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-08-14 18:54:32 +0900

    Use timestamp for save and load

--- save_load.py
+++ save_load.py
@@ -1,7 +1,14 @@
-"""Save and load trained PyTorch models."""
+"""Save and load trained PyTorch models.
+
+A directory is given for save since multiple models
+can be saved in one run, but a path (including filname)
+is given for load since only one model is loaded.
+"""
 import os
 
 import torch
+
+from .timestamp import get_timestamp
 
 
 def load_models(load_path: str, **kwargs):
@@ -18,18 +25,30 @@ def load_models(load_path: str, **kwargs):
         value.load_state_dict(state_dict[key])
 
 
-def save_models(save_path: str, **kwargs):
+def save_models(save_dir: str, suffix: str = "net", **kwargs):
     """Save specified models.
 
     Parameters
     ----------
-    save_path : str
-        Save path including the filename.
+    save_dir : str
+        Save directory, not excluding the filename.
+    suffix : str
+        Savefile suffix after timestamp.
+
+    Returns
+    -------
+    unique_save_dir : str
+        The unique directory with timestamp, generated
+        inside the specified save directory.
 
     """
     # Create specified directory if it does not exist yet
-    save_dir = "/".join(save_path.split("/")[:-1])
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    unique_save_dir = save_dir + f"/{get_timestamp()}/"
+    unique_save_path = save_dir + f"/{get_timestamp()}/{suffix}.pt"
+    if not os.path.exists(unique_save_dir):
+        os.makedirs(unique_save_dir)
 
-    torch.save({key: value.state_dict() for key, value in kwargs.items()}, save_path)
+    torch.save(
+        {key: value.state_dict() for key, value in kwargs.items()}, unique_save_path
+    )
+    return unique_save_dir
