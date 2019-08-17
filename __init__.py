c272e65bfbfbc95322921f912c63c214ad962bd5
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-08-17 12:37:03 +0900

    Format with black

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 """Methods commonly used in any paper implementations."""
 from .anneal import get_linear_anneal_func  # noqa: F401
 from .logger import get_logger  # noqa: F401
-from .reproducibility import set_global_random_seeds, set_env_random_seeds  # noqa: F401
+from .reproducibility import set_env_random_seeds, set_global_random_seeds  # noqa: F401
 from .save_load import load_models, save_models  # noqa: F401
 from .timestamp import get_timestamp  # noqa: F401
